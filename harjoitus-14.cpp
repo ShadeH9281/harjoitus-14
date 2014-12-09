8922202c70c2d99ccfae3f4fdc860ab256071d6d
ShadeH9281 <ffk7shade@gmail.com>
2014-12-09 13:05:38 +0200

    Palautus harjoitus-14

---
+++ harjoitus-14.cpp
@@ -0,0 +1,19 @@
+//Made by Toni Pajukanta,Ryhmä IIO14S1
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+struct name
+{
+
+	char etunimet[100], suku[100], koko[40];
+	string nick, king, full;
+
+};
+
+void main()
+{
+	name one;
+
+	
